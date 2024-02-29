536f80dbd4e65ebb5964d51b24de26dc879d5e0f
hyching21 <sheep128763@gmail.com>
2024-03-01 01:42:43 +0800

    upload test file

---
+++ app.py
@@ -0,0 +1,60 @@
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookParser, WebhookHandler
+from linebot.exceptions import InvalidSignatureError, LineBotApiError
+from linebot.models import*
+from settings import*
+from testfunc import*
+app = Flask(__name__)
+
+
+try: 
+    line_bot_api = LineBotApi(channel_access_token)
+    parser = WebhookParser(channel_secret)
+    handler = WebhookHandler(channel_secret)
+except:
+    print("Error while connecting to LineBot")
+    
+
+# 監聽所有來自 /callback 的 Post Request
+@app.route("/callback", methods=['POST'])
+def callback():
+    global chat_button
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+
+    try:
+        events = parser.parse(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    except LineBotApiError:
+        abort(400) 
+    
+    for event in events:
+        if isinstance(event, MessageEvent):
+            if event.message.text == "你好":
+                line_bot_api.reply_message(
+                    event.reply_token,TextSendMessage(text="想查詢甚麼呢?")
+                )
+            elif event.message.text == "@quick":
+                quicktest(event)
+            elif event.message.text == "@button":
+                buttontest(event)
+            elif event.message.text == "@carousel":
+                carouseltest(event)    
+            else:
+                line_bot_api.reply_message(
+                    event.reply_token,TextSendMessage(text=event.message.text)
+                )
+                
+#訊息傳遞區塊
+# @handler.add(MessageEvent, message=TextMessage)
+# def handle_message(event):
+#     message = TextSendMessage(text=event.message.text)
+#     line_bot_api.reply_message(event.reply_token,message)
+
+# main
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 80))
+    app.run(host='0.0.0.0', port=port)
+
---
+++ testfunc.py
@@ -0,0 +1,94 @@
+from linebot import LineBotApi,WebhookParser
+from linebot.models import *
+from settings import *
+
+line_bot_api = LineBotApi(channel_access_token)
+parser = WebhookParser(channel_secret)
+
+def quicktest(event):
+    try:
+        t=TextSendMessage(
+            text='quick reply 按鈕測試',
+            quick_reply=QuickReply(
+                items=[
+                    QuickReplyButton(
+                        action=MessageAction(
+                            label='message',
+                            data='action=quick & item=1',
+                            text='MessageAction'
+                        )
+                    ),
+                    QuickReplyButton(
+                        action=URIAction(
+                            label='open URL',
+                            uri="https://www.google.com.tw/"
+                        )
+                    )
+                ]
+            )
+        )
+        line_bot_api.reply_message(event.reply_token, t)  
+    except Exception as e:
+        print(f"Error in quicktest: {e}")
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text='失敗'))
+
+def buttontest(event):
+    try:
+        t=TemplateSendMessage(
+            alt_text='Buttons template',
+                template=ButtonsTemplate(
+                    title='Button 按鈕測試',
+                    text='請選擇功能',
+                    actions=[
+                        MessageTemplateAction(
+                            label='功能1',
+                            text='text1'
+                        ),
+                        MessageTemplateAction(
+                            label='功能2',
+                            text='text2'
+                        ),
+                        MessageTemplateAction(
+                            label='功能3',
+                            text='text3'
+                        )
+                    ]
+                )
+        )
+        line_bot_api.reply_message(event.reply_token,t)
+    except Exception as e:
+        print(f"Error in quicktest: {e}")
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text='失敗')) 
+
+def carouseltest(event):
+    try:
+        t=TemplateSendMessage(
+            alt_text='Carousel template',
+            template=CarouselTemplate(
+                columns=[
+                    CarouselColumn(
+                        thumbnail_image_url='https://images.pexels.com/photos/6686455/pexels-photo-6686455.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+                        title='carousel 按鈕測試',
+                        text='選擇一個分類吧',
+                        actions=[
+                            PostbackAction(
+                                label='分類一',
+                                data='action=carouseltest'
+                            ),
+                            PostbackAction(
+                                label='分類二',
+                                data='action=carouseltest'
+                            ),
+                            PostbackAction(
+                                label='分類三',
+                                data='action=carouseltest'
+                            )
+                        ]
+                    )
+                ]
+            )
+        )
+        line_bot_api.reply_message(event.reply_token,t)
+    except Exception as e:
+        print(f"Error in quicktest: {e}")
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text='失敗')) 
